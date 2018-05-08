999f1f25bbae0ae045dadf0870ab3b1a2b4d301b
jakubdrewniak <drewniak.jakub@gmail.com>
2018-05-08 21:28:56 +0200

    scroll to top button added.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+
+
+
+$( document ).ready(function() {
+
 /*-----------------------------------------------
 *   script by w3schools
 https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
 changed line "scrollTop: $(hash).offset().top " to "scrollTop: $(hash).offset().top - 80" to prevent scrolling to low
 deleted window.location.hash
 -----------------------------------------------*/
-
-
-$( document ).ready(function() {
 
     // Add scrollspy to <body>
     $('body').scrollspy({target: ".navbar", offset: 100});
@@ -28,9 +30,30 @@ $( document ).ready(function() {
         $('html, body').animate({
           scrollTop: $(hash).offset().top - 80
         }, 400);
-
       } // End if
-
     });
+
+
 });
 
+/*-----------------------------------------------
+*   script by w3schools
+https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+-----------------------------------------------*/
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
